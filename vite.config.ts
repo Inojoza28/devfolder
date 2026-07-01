f6ba6f4f31f4538a53c27d8f25b54ee183230de2
gabrielpaguebembrasil <gabrielpaguebembrasil@gmail.com>
2026-07-01 14:31:13 -0300

    Ajuste para vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
-  plugins: [
-    ...tanstackStart({
-      server: { entry: "server" },
-    }),
-    react(),
-    tsconfigPaths(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    tsconfigPaths: true,
+  },
   server: {
     port: 8080,
   },
